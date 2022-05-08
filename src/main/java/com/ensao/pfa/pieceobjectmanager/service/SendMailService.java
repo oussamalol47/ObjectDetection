d8bb0d9d852976e157796f2338adeafe2ee0679e
Benziza <user.name>
2022-05-08 22:55:00 +0000

    make it better

--- src/main/java/com/ensao/pfa/pieceobjectmanager/service/SendMailService.java
+++ src/main/java/com/ensao/pfa/pieceobjectmanager/service/SendMailService.java
@@ -24,7 +24,7 @@ public class SendMailService {
     public Mail sendMail(Mail mail) throws MailException{
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo("benizizamohamed@gmail.com");
-        msg.setFrom(mail.getExpediteur());
+        msg.setFrom("mohamed.benziza@ump.ac.ma");
         msg.setSubject(mail.getObjet());
         msg.setText(mail.getMessage());
         javaMailSender.send(msg);
